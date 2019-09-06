b771be3ff2238b7e1b634d116fdc187e8e1c8f02
ubervelocity <mihaitu.popescu@gmail.com>
2019-09-06 17:20:07 +0300

    init c# projects

---
+++ hello/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass c1 = new MyClass();
+            Console.WriteLine($"Hello World! {c1.ReturnMessage()}");
+        }
+    }
+}
